c6d88166c6a74daf96642d24fa6d656f568e154c
Chia-Che Tsai <chiache@tamu.edu>
2021-02-24 01:40:49 +0800

    Add hints for cloud APIs

--- cloud.py
+++ cloud.py
@@ -13,6 +13,15 @@ class AWS_S3(cloud_storage):
         self.bucket_name = ""
 
     # Implement the abstract functions from cloud_storage
+    # Hints: Use the following APIs from boto3
+    #     boto3.session.Session:
+    #         https://boto3.amazonaws.com/v1/documentation/api/latest/reference/core/session.html
+    #     boto3.resources:
+    #         https://boto3.amazonaws.com/v1/documentation/api/latest/guide/resources.html
+    #     boto3.s3.Bucket:
+    #         https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html#bucket
+    #     boto3.s3.Object:
+    #         https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html#object
 
 class Azure_Blob_Storage(cloud_storage):
     def __init__(self):
@@ -26,15 +35,30 @@ class Azure_Blob_Storage(cloud_storage):
         self.container_name = ""
 
     # Implement the abstract functions from cloud_storage
+    # Hints: Use the following APIs from azure.storage.blob
+    #    blob.BlobServiceClient:
+    #        https://docs.microsoft.com/en-us/python/api/azure-storage-blob/azure.storage.blob.blobserviceclient?view=azure-python
+    #    blob.ContainerClient:
+    #        https://docs.microsoft.com/en-us/python/api/azure-storage-blob/azure.storage.blob.containerclient?view=azure-python
+    #    blob.BlobClient:
+    #        https://docs.microsoft.com/en-us/python/api/azure-storage-blob/azure.storage.blob.blobclient?view=azure-python
 
 class Google_Cloud_Storage(cloud_storage):
     def __init__(self):
+        # Google Cloud Storage is authenticated with a **Service Account**
         # TODO: Download and place the Credential JSON file
         self.credential_file = "gcp-credential.json"
         # TODO: Fill in the container name
         self.bucket_name = ""
 
     # Implement the abstract functions from cloud_storage
+    # Hints: Use the following APIs from google.cloud.storage
+    #    storage.client.Client:
+    #        https://googleapis.dev/python/storage/latest/client.html
+    #    storage.bucket.Bucket:
+    #        https://googleapis.dev/python/storage/latest/buckets.html
+    #    storage.blob.Blob:
+    #        https://googleapis.dev/python/storage/latest/blobs.html
 
 class RAID_on_Cloud(NAS):
     def __init__(self):
