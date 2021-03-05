4011380a494205f3c2cfeea6561161cd055217dd
Chia-Che Tsai <chiache@tamu.edu>
2021-03-05 23:31:12 +0800

    Update autograder scripts

--- autograder/setup.sh
+++ autograder/setup.sh
@@ -2,7 +2,7 @@ apt update && apt install -y python python-pip
 pip install gradescope-utils>=0.3.1 boto3 google-cloud-storage azure-storage-blob
 
 cd /autograder
-git clone https://github.com/chiache/csce678-s21-project1 grading
+git clone https://github.com/chiache/csce678-s21-project1 -b autograder grading
 cp /autograder/source/run_tests.py /autograder/grading/run_tests.py
 
 cd /autograder/grading
